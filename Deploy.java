f7d390de27f9659d10bb8a9070ebd5dab04312df
Fidaarooj <aroojfida57@gmail.com>
2022-10-25 20:45:11 +0530

    Updated the deployment file

--- Deploy.java
+++ Deploy.java
@@ -1,4 +1,7 @@
  Hello , This file contains the deployment script
+<<<<<<< HEAD
  Updated the content
-
  MOdified this file for other commands
+=======
+>>>>>>> parent of e88b603 (Updated)
+	>>>>>>> updating this file again
