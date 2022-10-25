d5c5a3f5ce121b698fc15505ca6de408c297d43f
Fidaarooj <aroojfida57@gmail.com>
2022-10-25 20:47:30 +0530

    Reverted "Updated the deployment file"
    This reverts commit f7d390de27f9659d10bb8a9070ebd5dab04312df.

--- Deploy.java
+++ Deploy.java
@@ -1,7 +1,4 @@
  Hello , This file contains the deployment script
-<<<<<<< HEAD
  Updated the content
+
  MOdified this file for other commands
-=======
->>>>>>> parent of e88b603 (Updated)
-	>>>>>>> updating this file again
